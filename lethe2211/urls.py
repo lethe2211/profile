75a899f6ad300b9d04b0ba65c6c742e81252a524
lethe <lethe2211@gmail.com>
2016-02-07 11:40:55 +0900

    Process root URL

--- lethe2211/urls.py
+++ lethe2211/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 
 admin.autodiscover()
@@ -9,7 +8,8 @@ urlpatterns = [
     # url(r'^$', 'lethe2211.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('introduction.urls', namespace='introduction')),    
     url(r'^introduction/', include('introduction.urls', namespace='introduction')),
 ]
 
